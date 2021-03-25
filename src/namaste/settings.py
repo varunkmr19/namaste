5d2c50155908d32546c1e0f3e4e400b87692b340
varunkmr19 <varun-kumar@live.com>
2021-03-25 12:28:21 +0530

    Setup base templates

--- src/namaste/settings.py
+++ src/namaste/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'namaste.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,5 +133,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 STATIC_URL = '/static/'
