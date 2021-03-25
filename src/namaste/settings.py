e6914c7c49fe0aa1769b6604ed65b96f2bb6f194
varunkmr19 <varun-kumar@live.com>
2021-03-25 17:19:31 +0530

    Add bootstrap and config static files

--- src/namaste/settings.py
+++ src/namaste/settings.py
@@ -136,6 +136,13 @@ USE_TZ = True
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'media')
 ]
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
+
+TEMP = os.path.join(BASE_DIR, 'media_cdn/temp')
